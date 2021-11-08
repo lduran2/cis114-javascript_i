e24688122d640f8a4a34393684cb8c1f16d0e659
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-08 17:40:56 -0500

    [page_fixer] size Mars image

--- page_fixer/fixer.js
+++ page_fixer/fixer.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // Your code goes here!
 
     const MARS_FILENAME = 'mars.jpg';   // Mars image file name
+    const MARS_WIDTH = 500;             // [px]
+    const MARS_HEIGHT = 500;            // [px]
 
     // get the first image element
     const imgEl0 = document.querySelector('img');   // image element
@@ -14,8 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const MARS_FILE = new Image();      // the Mars file
     MARS_FILE.src = MARS_FILENAME;
     MARS_FILE.addEventListener('load', function (){
-        imgEl0.src = this.src;
+        // update the image source
+        imgEl0.setAttribute('src', this.src);
     });
+
+    // size the image element
+    imgEl0.setAttribute('width', `${MARS_WIDTH}`);
+    imgEl0.setAttribute('height', `${MARS_HEIGHT}`);
 
     console.log('Done.');
 
