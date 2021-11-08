5198f3ff5548c37e65f2aaac54c66567fd98cf51
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-08 16:20:42 -0500

    [page_fixer] downloaded start, goal pages

---
+++ page_fixer/fixer.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Leave this line alone.
+document.addEventListener("DOMContentLoaded", function() {
+
+    //// Remove the alert and then get started.
+    alert('The HTML of this page is broken. ' +
+        'You need to write some JavaScript to put it back together.');
+
+    // Your code goes here!
+
+
+// Leave this line alone.
+});
