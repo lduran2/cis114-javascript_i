0b64f8afe6b6f5c8a07277a8d54a010503da8e82
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-08 17:05:44 -0500

    [page_fixer] loaded Mars image

--- page_fixer/fixer.js
+++ page_fixer/fixer.js
@@ -3,11 +3,18 @@
 // Leave this line alone.
 document.addEventListener("DOMContentLoaded", function() {
 
-    //// Remove the alert and then get started.
-    alert('The HTML of this page is broken. ' +
-        'You need to write some JavaScript to put it back together.');
-
     // Your code goes here!
+
+    const MARS_FILENAME = 'mars.jpg';   // Mars image file name
+
+    // load the Mars image
+    const MARS_FILE = new Image();      // the Mars file
+    MARS_FILE.src = MARS_FILENAME;
+    MARS_FILE.addEventListener('load', function (){
+        alert(`Loaded the image ${this.src}!`);
+    });
+
+    console.log('Done.');
 
 
 // Leave this line alone.
