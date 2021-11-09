7ba58353ba7ae0072880e5ab5da5414ace99834d
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-08 19:06:14 -0500

    [page_fixer] found heading of knowledge list

--- page_fixer/fixer.js
+++ page_fixer/fixer.js
@@ -8,17 +8,28 @@ document.addEventListener("DOMContentLoaded", function() {
     //-----------------------------------------------------------------
     // constants
     //-----------------------------------------------------------------
+
+    // for main-title
     const TO_EMPH = 'this';  // word to emphasize in main title
 
+    // for the Mars image
     const MARS_FILENAME = 'mars.jpg';   // Mars image file name
     const MARS_WIDTH = 500;             // [px]
     const MARS_HEIGHT = 500;            // [px]
 
+    // for the ordered list
     const OL0_NEW_CLASS = 'engage';     // class to add to first
                                         // ordered list
 
+    // for the knowledge list
+    const H3_EL = document.createElement('h3'); // a level 3 heading
+                                                // element
+    const H3_TAGNAME = H3_EL.tagName;   // its exptected tag name
+    const KNOWLEDGES_HEAD_APPEND = 'about JavaScript';  // string to
+                                                // add to the heading
+
     //-----------------------------------------------------------------
-    // update the main title
+    // add emphasis to the main title
     //-----------------------------------------------------------------
 
     // get the main title
@@ -48,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // create and insert the next split
         const NEXT_SPLIT_TEXT = document.createTextNode(SPLIT);
         MAIN_TITLE_EL.insertBefore(NEXT_SPLIT_TEXT, NEW0_MAIN_TITLE);
-    } // for (const SPLIT of EMPH_SPLITS)
+    } // end for (const SPLIT of EMPH_SPLITS)
 
     //-----------------------------------------------------------------
     // update the first image
@@ -70,13 +81,28 @@ document.addEventListener("DOMContentLoaded", function() {
     IMG_EL0.setAttribute('height', `${MARS_HEIGHT}`);
 
     //-----------------------------------------------------------------
-    // update the first ordered list
+    // update the first ordered list's class list
     //-----------------------------------------------------------------
-
     // get the first ordered list
     const OL_EL0 = document.querySelector('ol');
     // change the class of the ordered list
     OL_EL0.classList.add(OL0_NEW_CLASS);
+
+    //-----------------------------------------------------------------
+    // append to the text of level 3 heading
+    //-----------------------------------------------------------------
+
+    // find the heading before the knowledge list
+    // get the knowledge list
+    const KNOWLEDGES_EL = document.querySelector('#knowledge-list');
+    // find the previous heading node
+    let knowledges_head_el = KNOWLEDGES_EL;
+    // while not a heading element
+    while (H3_TAGNAME != knowledges_head_el.tagName) {
+        // update to the previous sibling
+        knowledges_head_el = knowledges_head_el.previousSibling;
+    } // end while (H3_TAGNAME != knowledges_head_el.tagName)
+    console.log(knowledges_head_el);
 
     console.log('Done.');
 
