ab971efb7484301737b4fda7b5822307fd2c2ade
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-12-03 21:36:55 -0500

    [ajax_request] tested multiple forms

--- scripts/populate_json_forms.js
+++ scripts/populate_json_forms.js
@@ -3,13 +3,19 @@
  * Gets data from the JSON action of each JSON form and populates them.
  *
  * By        : Leomar Duran <https://github.com/lduran2>
- * When      : 2021-12-03t20:01
+ * When      : 2021-12-03t21:30
  * Where     : Community College of Philadelphia
  * For       : CIS 114/JavaScript I
- * Version   : 1.1.0
+ * Version   : 1.1.2
  * Canonical : https://github.com/lduran2/cis114-javascript_i/blob/master/scripts/populate_json_forms.js
  *
  * CHANGELOG :
+ *     v1.1.2 - 2021-12-03t21:30
+ *         tested multiple forms
+ *
+ *     v1.1.1 - 2021-12-03t21:14
+ *         testing `REQUEST.onerror`
+ *
  *     v1.1.0 - 2021-12-03t20:01
  *         started populating multiple forms
  *
@@ -18,13 +24,14 @@
  */
 'use strict';
 
+/** enumeration of the requestType for JSON files */
+const JSON_TYPE = 'json';
+
 /**
  * Gets data from the JSON action of each JSON form and populates them.
  * @param evnt : Event = the event that triggers this listener
  */
 function main(evnt) {
-    const JSON_TYPE = 'json';
-    const REQUEST = new XMLHttpRequest();
     /* get and check the document document body */
     const BODY_EL = document.querySelector('body');
     if (!BODY_EL) {
@@ -36,25 +43,36 @@ function main(evnt) {
     const JSON_FORM_ELS = BODY_EL.querySelectorAll(JSON_FORM_SELECTOR);
     /* loop through them */
     for (const FORM_EL of JSON_FORM_ELS) {
-        /* get the form action and method */
-        const METHOD = FORM_EL.getAttribute('method');
-        const ACTION = FORM_EL.getAttribute('action');
-        /* open the request */
-        REQUEST.open(METHOD, 'https://developer.mozilla.org/');
-        REQUEST.responseType = JSON_TYPE;
-        /* add the load and error events */
-        REQUEST.addEventListener('load', createPopulateForm(FORM_EL));
-        REQUEST.addEventListener('error', throwRequestLoadingError);
-        /* send the request */
-        REQUEST.send();
+        sendJsonFormRequest(FORM_EL);
     } /* end for (const FORM_EL of JSON_FORM_ELS) */
 
     /* finish */
     console.log('Done.');
-} /* end function main() */
+} /* end function main(evnt) */
 
 /**
- * Creates an event that populates the given form element.
+ * Sends a request to a JSON file using the given form element's method
+ * and action.
+ * @param formEl : Element = for which to send a JSON request
+ */
+function sendJsonFormRequest(formEl) {
+    /* create a new request */
+    const REQUEST = new XMLHttpRequest();
+    /* get the form action and method */
+    const METHOD = formEl.getAttribute('method');
+    const ACTION = formEl.getAttribute('action');
+    /* open the request */
+    REQUEST.open(METHOD, ACTION);
+    REQUEST.responseType = JSON_TYPE;
+    /* add the load and error events */
+    REQUEST.addEventListener('load', createPopulateForm(formEl));
+    REQUEST.addEventListener('error', throwRequestLoadingError);
+    /* send the request */
+    REQUEST.send();
+} /* function sendJsonFormRequest(formEl) */
+
+/**
+ * Creates an event listener that populates the given form element.
  * @param evnt : Event = the event that triggers this listener
  */
 function createPopulateForm(formEl) {
@@ -63,8 +81,10 @@ function createPopulateForm(formEl) {
         console.log('createPopulateForm(formEl)(evnt)');
         console.log('formEl:', formEl);
         console.log('evnt:', evnt);
+        /* get the response data */
+        const DATA = evnt.target.response;
         /* expand the response */
-        console.dir(evnt.target.response);
+        console.dir(DATA);
     }; /* return function (evnt) */
 } /* end function createPopulateForm(formEl) */
 
@@ -76,6 +96,7 @@ function throwRequestLoadingError(evnt) {
     /* log the event */
     console.log('throwRequestLoadingError(evnt)');
     console.log('evnt:', evnt);
+    /* stop the program */
     throw 'error loading request';
 } /* end function throwRequestLoadingError(evnt) */
 
